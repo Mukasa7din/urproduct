b24ec513fc08add499298bf79b6deeb85fab31cd
mukasa7din <mukasa7din@github.io>
2019-09-26 23:43:53 -0700

    Updates

---
+++ precache-manifest.bd9457e5856afd5d5a0ff29dbbea5160.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d009b0478e706329425310aba61906c8",
+    "url": "/urproduct/index.html"
+  },
+  {
+    "revision": "42fc652e48f1ce5c07aa",
+    "url": "/urproduct/static/css/main.5136facd.chunk.css"
+  },
+  {
+    "revision": "253775876ff3324964e3",
+    "url": "/urproduct/static/js/2.75e8dc7f.chunk.js"
+  },
+  {
+    "revision": "42fc652e48f1ce5c07aa",
+    "url": "/urproduct/static/js/main.90ab15d8.chunk.js"
+  },
+  {
+    "revision": "62464e831605ecb0dbc4",
+    "url": "/urproduct/static/js/runtime~main.14956f62.js"
+  },
+  {
+    "revision": "c2383da907130ac28dd4b1ff235cc712",
+    "url": "/urproduct/static/media/Day breaks.c2383da9.jpeg"
+  },
+  {
+    "revision": "e51813b7f7733f84617ff55bbc396bce",
+    "url": "/urproduct/static/media/Spring shirt.e51813b7.jpeg"
+  },
+  {
+    "revision": "3017338c3d2a32c9fc0d2b972deb393c",
+    "url": "/urproduct/static/media/Spring short.3017338c.jpeg"
+  },
+  {
+    "revision": "3dcd7ed5cd8e16017a3574e0b8ffd235",
+    "url": "/urproduct/static/media/Suite Vest.3dcd7ed5.jpeg"
+  },
+  {
+    "revision": "ac2fce1d4a67e2a0a1914c80bc7096bf",
+    "url": "/urproduct/static/media/Summer Vest.ac2fce1d.jpeg"
+  },
+  {
+    "revision": "65d8d5fa9e4bab620dc88acbda0ef2dc",
+    "url": "/urproduct/static/media/Vetom shirts.65d8d5fa.jpeg"
+  },
+  {
+    "revision": "db0923e97ad590c2114de993529c6bf1",
+    "url": "/urproduct/static/media/beach ware.db0923e9.jpeg"
+  },
+  {
+    "revision": "147b892cb3362bb645c7ddfda442d969",
+    "url": "/urproduct/static/media/men-wear.147b892c.jpeg"
+  },
+  {
+    "revision": "ce14f3b1696034296fab97356e1b0ab7",
+    "url": "/urproduct/static/media/summer plots.ce14f3b1.jpeg"
+  }
+]);
